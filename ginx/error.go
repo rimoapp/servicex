aa753a21561741b7cb0398d94ce0803389ccdb6b
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2020-06-19 15:39:27 +0900

    Affect update of rimo-backend

--- ginx/error.go
+++ ginx/error.go
@@ -2,15 +2,38 @@ package ginx
 
 import (
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/getsentry/sentry-go"
 	sentrygin "github.com/getsentry/sentry-go/gin"
 	"github.com/gin-gonic/gin"
-	"github.com/kpango/glg"
+	"github.com/pkg/errors"
+
+	"github.com/rimoapp/rimo-backend/lib/logx"
 )
 
+var closers []func()
+
+func Init(dsn string) {
+	if err := sentry.Init(sentry.ClientOptions{
+		Dsn:         dsn,
+		Environment: os.Getenv("APP_ENV"),
+	}); err != nil {
+		panic(err)
+	}
+
+	closers = append(closers, func() { sentry.Flush(30 * time.Second) })
+}
+
+func Close() {
+	for _, f := range closers {
+		f()
+	}
+}
+
 func ErrorSetStatus(ctx *gin.Context, status int, err error) {
-	glg.Error(err)
+	logx.LogError(ctx, err.Error())
 
 	hub := sentrygin.GetHubFromContext(ctx)
 	if hub == nil {
@@ -29,13 +52,11 @@ func Error(ctx *gin.Context, err error) {
 	ErrorSetStatus(ctx, http.StatusInternalServerError, err)
 }
 
-func NoticeOnlyError(ctx *gin.Context, err error) {
-	glg.Error(err)
-
+func NoticeOnlyError(ctx *gin.Context, msg string) {
 	hub := sentrygin.GetHubFromContext(ctx)
 	if hub == nil {
 		hub = sentry.CurrentHub()
 	}
 
-	hub.CaptureException(err)
+	hub.CaptureException(errors.New(msg))
 }
