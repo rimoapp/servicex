9fd095f958736a01805dc2c38b2af065fef6b04a
Naoyoshi Aikawa <n.aikawa91@gmail.com>
2021-07-12 22:52:37 +0900

    Add DefautGinEngine function

---
+++ gin.go
@@ -0,0 +1,18 @@
+package servicex
+
+import (
+	sentrygin "github.com/getsentry/sentry-go/gin"
+	"github.com/gin-gonic/gin"
+)
+
+// DefaultGinEngine returns gin engine with needed middleware
+func DefaultGinEngine() *gin.Engine {
+	router := gin.Default()
+
+	// Send request infomation to sentry when panic
+	router.Use(sentrygin.New(sentrygin.Options{
+		Repanic: true,
+	}))
+
+	return router
+}
